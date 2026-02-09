098fefdb7698cd5c0623a64b027ccb821bf560e8
liyoyoapp <zy.li.apply@gmail.com>
2026-02-09 17:03:25 +0800

    update member model

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, verbose_name='First Name')
+    last_name = models.CharField(max_length=100, verbose_name='Last Name')
     phone_number = models.IntegerField(null=True)
+
+    #admin page Member object (1) to the string below
+    def __str__(self):
+        return f'{self.first_name} -  {self.last_name}'
+
+    #super save function: add extra words when saving
+    def save(self,*args,**kwargs):
+        self.first_name = f"Mr./Ms.{self.first_name}"
+        super().save(*args,**kwargs)
