c5720a3dc9e4a33da38581b46090974c913d13a7
liyoyoapp <zy.li.apply@gmail.com>
2026-02-09 16:37:02 +0800

    add phone number field in members updated 2

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    phone_number = models.IntegerField(null=True)
