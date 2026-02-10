8530834342bfbad0f54712a3813e0bcc37b5cbfe
liyoyoapp <zy.li.apply@gmail.com>
2026-02-10 11:07:00 +0800

    2026-02-10 AM update 1 +views related

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def members(request):
-    return HttpResponse("Hello, world. You're at the Member page.")
+    context = {
+        "first_name": "ABC",
+        "last_name": "Li",
+    }
+    return render(request, 'members/index.html',context)
+    #return HttpResponse("Hello, world. You're at the Member page.")
--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'members.apps.MembersConfig',
+    'officer',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'my_tennis_club.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
